f75d293ab2e98baa7c71419520f98f84c0cf0434
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2021-05-21 21:25:39 -0300

    21/05/2021

---
+++ Aula 2/Exercício2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main (void){
+
+    int i = 0;
+    float scores[3], average;
+    for(i = 0; i <= 2; i++){
+        printf("type the score %i: ", i);
+        scanf("%f", &scores[i]);
+        average = average + (scores[i]/3);
+    }
+
+    if(average < 6){
+        printf("Aluno reprovado.");
+    }
+    else{
+        printf("Aluno aprovado.");
+    }
+}
