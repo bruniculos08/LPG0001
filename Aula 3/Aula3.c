02fd1b07a7e8671cb968caee5f092b2080abb0ca
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2021-05-24 13:59:08 -0300

    Create Aula3.c

---
+++ Aula 3/Aula3.c
@@ -0,0 +1,34 @@
+/* deixar último ganchinho com roupa para não bater a porta
+& Repetição por While
+
+while(condição){
+    bloco de comandos;
+}
+
+* O laço se repete enquanto a condição for verdadeira.
+* Testa a condição antes de entrar; se for inicialmente falsa, não executa o laço nenhuma vez.
+* Dentro do laço deve haver atualização da variável que controla o laço, a fim de que não se torne um laço infinito
+? perceba que o While testa primeiro a condição antes de executar o comando, logo em certos casos é possível que os comandos não sejam executados nenhuma vez se quer 
+
+& Repetição por Do-While 
+
+do {
+    bloco de comandos;
+} 
+while (condição);
+
+* O laço se repete até que a condição se torne falsa.
+* Testa a condição ao final do laço, executando ao menos uma vez todos os comandos. 
+? perceba que o do-While executa e testa depois, ou seja, pelo menos uma vez os comandos serão executados
+
+& Repetição por For
+
+
+*/
+
+//^ Exemplo de While
+
+//^ Exemplo de Do-While
+
+//^ Exemplo de For
+
