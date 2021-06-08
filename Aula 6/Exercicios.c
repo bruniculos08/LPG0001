b09a0b0f4b53b1556add53270f12e5b4cbc7609c
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2021-06-08 00:22:47 -0300

    update

--- Aula 6/Exercicios.c
+++ Aula 6/Exercicios.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <string.h> 
+#include <stdlib.h>
 
 /* 
 • Fazer um programa para ler um vetor (matriz
@@ -63,14 +64,39 @@ int scalar_prod(void){
     {   
         scanf("%d", vector1[i]);
     }
-    
+
     printf("Type the vector2: ");
     for (int i = 0; i < 5; i++)
     {   
         
         scanf("%d", vector2[i]);
     }
-
+    
     for (int i = 0; i < 5; i++) produto_escalar += (vector1[i]*vector2[i]);
     printf("Produto Escalar (scalar product) = %f", produto_escalar);
+}
+
+/* 
+Faça um programa para ler uma frase e
+exibi-la na vertical (uma letra por linha). 
+*/
+
+int vertical_phrase(char phrase[]){
+    int n = strlen(phrase);
+    for (int i = 0; i < n; i++){
+        printf("%c\n", phrase[i]);
+    }
+    return 0;
+}
+
+int multiplica_matriz(int matriz1[4][3], int matriz2[3][4]){
+    return 0;
+}
+
+
+int main(void){
+    char phrase[1000];
+    int a = strlen(phrase);
+    gets(phrase); 
+    vertical_phrase(phrase);
 }
