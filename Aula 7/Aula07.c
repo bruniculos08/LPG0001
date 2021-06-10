3723f758c9443bc00337d58f7b835ae7df98a68e
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2021-06-09 21:04:13 -0300

    update

--- Aula 7/Aula07.c
+++ Aula 7/Aula07.c
@@ -1,15 +1,18 @@
 /* 
 & Ponteiros
 
-• Definição:
+? Definição:
 – Em programação, um ponteiro ou apontador é um tipo de dado
 de uma linguagem de programação cujo valor se refere
-diretamente a um outro valor alocado em outra área da memória,
-através de seu endereço. Um ponteiro é uma simples
-implementação do tipo referência da Ciência da computação.
+diretamente a um outro valor alocado em outra área da memória, através de seu endereço.
+Um ponteiro é uma simples implementação do tipo referência da Ciência da computação.
 
 
-
+• Conceito similar ao trabalhar com vetores:
+int i;
+float vetor[10];
+for(i=0; i<10; i++)
+ vetor[i] = 0.0;
 
 todo IMPORTANTE RESUMO:
 
