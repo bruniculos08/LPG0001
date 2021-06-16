52f743918ef039b1eb656824ec6cdfb63f877b39
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2021-06-16 16:06:46 -0300

    update

--- Trabalhos/Trabalho01/Exercicio04.c
+++ Trabalhos/Trabalho01/Exercicio04.c
@@ -1,35 +1,30 @@
 #include <stdio.h>
 #include <math.h>
-
-long double poison(long int k);
-double fat(int k);
-
-long double poison(long int k){
+long double poison(long double k);
+long double fat(long double k);
+long double poison(long double k){
     long double y = 300;
     long double e = 2.71828;
-    long double pot1 = powl((1.0000/e), y);
+    long double pot1 = expl(-y);
     long double pot2 = powl(y, k);
     long double pro1 = pot1*pot2;
-    long int fatk = fat(k);
+    long double fatk = fat(k);
     long double P = (pro1)/(fatk);
     return P;
 }
-
 // y = numero de ocorrencias esperadas, nesse caso, em média 300 lampadas por dia 
 // k = numero de ocorrencia que se queira verificar a possibilidade, sendo 290 ou 320 no proximo dia
-
-double fat(int k){
-    double result;
-    unsigned long long int i;
+long double fat(long double k){
+    long double result;
+    long double i;
     for(i = 1; i <= k; i++ ) {
         result *= i;
     }
     return result;
 }
-
 int main(){
-    printf("fat(k) = %lli \n", fat(290)); 
-    printf("P(2, 300) = %Le \n", poison(1)); 
-    printf("P(290, 300) = %Le \n", poison(290));
-    printf("P(320, 300) = %Le \n", poison(320));
+    printf("fat(k) = %Lf \n", fat(290)); 
+    printf("P(2, 300) = %Lf \n", poison(1)); 
+    printf("P(290, 300) = %Lf \n", poison(290));
+    printf("P(320, 300) = %Lf \n", poison(320));
 }
