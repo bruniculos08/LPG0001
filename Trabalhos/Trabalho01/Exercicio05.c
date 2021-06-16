386d2a6a8668ba6926e8eecc0261c2b2c5838d05
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2021-06-15 22:07:59 -0300

    Update Exercicio05.c

--- Trabalhos/Trabalho01/Exercicio05.c
+++ Trabalhos/Trabalho01/Exercicio05.c
@@ -7,4 +7,15 @@ int main(){
 
     q = (p/j)*(1-powf(1+j,-n));
     printf("q = %.6lf", q);
+
+    if (fabs(q-(q/3.0)*3.0) < 0000.1){
+        q = (p/j)*(1-powf(1+j,-n));
+        printf("q = %.6lf", q);
+    }
+    else
+    {
+        q = (p/j)*(1-powf(1+j,-n));
+        printf("q = %.6lf (erro de precisao!)", q);
+    }
+    
 }
