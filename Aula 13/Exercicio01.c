33529556c13592ec5324b8b5c6f2e7ef808b6bca
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2021-08-02 14:21:28 -0300

    update

--- Aula 13/Exercicio01.c
+++ Aula 13/Exercicio01.c
@@ -13,14 +13,16 @@ int main(){
     strcpy(Teste.nome, "teste");
 
     FILE *saida;
-    saida = fopen("arquivotextosaida", "a+b");
+    saida = fopen("arquivotextosaida", "w+b"); //^Sobrescreve arquivo "arquivotextosaida", com endereço armazenado na variavel "saida" 
 
-    fseek(saida, 0, SEEK_SET);
-    fwrite(&Teste, sizeof(Estrutura), 1, saida); 
+    fseek(saida, 0, SEEK_SET);                  //^ coloca o leitor na posição 0 em relação ao início (SEEK_SET) 
+    fwrite(&Teste, sizeof(Estrutura), 1, saida);//^ Escreve o que está no endereço de Teste ou seja, a variável teste, que tem temanho de uma Estrutura,
+                                                //^ no arquivo de endereço armazenado em "saida"
 
-    Estrutura p;
-    fseek(saida, 0, SEEK_SET);
-    fread(&p, sizeof(Estrutura), 1, saida);
+    Estrutura p;                                //^ variável p do tipo estrutura
+    fseek(saida, 0, SEEK_SET);                  //^ coloca o leitor na posição 0 em relação ao início (SEEK_SET)
+    fread(&p, sizeof(Estrutura), 1, saida);     //^ copia para a variável "p" o que está sendo lido no arquivo de endereço armazenado em saida
+                                                //^ tendo esse algo o tamanho de uma estrutura (1)
 
-    printf("%s", p.nome);
+    printf("%s", p.nome);                       //^ printa algo(nome) da estrutura que foi copiada para a variável "p"
 }
